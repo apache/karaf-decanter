b4b5bb3a1369bd747a7558f6d48d3fca1a8316f7
mhautman <morgan.hautman@gmail.com>
2015-03-15 13:14:36 +0100

    Updated Elasticsearch appender. KARAF-3608

--- appender/elasticsearch/src/main/java/org/apache/karaf/decanter/appender/elasticsearch/Activator.java
+++ appender/elasticsearch/src/main/java/org/apache/karaf/decanter/appender/elasticsearch/Activator.java
@@ -16,36 +16,43 @@
  */
 package org.apache.karaf.decanter.appender.elasticsearch;
 
+import java.io.IOException;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.apache.karaf.decanter.api.Appender;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class Activator implements BundleActivator {
 
     private ElasticsearchAppender appender;
     private ServiceRegistration serviceReg;
+    private static final String CONFIG_PID = "org.apache.karaf.decanter.appender.elasticsearch";
 
     public void start(final BundleContext bundleContext) {
         // TODO embed mode of Elasticsearch
         Dictionary<String, String> properties = new Hashtable<>();
         properties.put("name", "elasticsearch");
-
-        serviceReg = bundleContext.registerService(Appender.class, appender, properties);
+        properties.put(Constants.SERVICE_PID, CONFIG_PID);
+        serviceReg = bundleContext.registerService(ManagedService.class.getName(), new ConfigUpdater(bundleContext),properties);
     }
 
     public void stop(BundleContext bundleContext) {
+        if(appender!=null)
         appender.close();
-        serviceReg.unregister();
     }
 
     private final class ConfigUpdater implements ManagedService {
 
+        private final Logger logger = LoggerFactory.getLogger(ConfigUpdater.class);
         private BundleContext bundleContext;
 
         public ConfigUpdater(BundleContext bundleContext){
@@ -61,13 +68,16 @@ public class Activator implements BundleActivator {
             }
 
             if (config == null) {
+                logger.debug("Elasticsearch appender with a default configuration is made with host : localhost and port : 9300");
                 appender = new ElasticsearchAppender("localhost", 9300);
             } else{
-                appender = new ElasticsearchAppender((String)config.get("host"), (Integer)config.get("port"));
+                logger.debug("Elasticsearch appender configured with a new Host : " + (String) config.get("host") + " and port : " + (String) config.get("port"));
+                appender = new ElasticsearchAppender((String)config.get("host"), Integer.parseInt((String)config.get("port")));
             }
             appender.open();
-            serviceReg = bundleContext.registerService(Appender.class, appender, config);
+            Dictionary<String, String> properties = new Hashtable<>();
+            properties.put("name", "elasticsearch");
+            serviceReg = bundleContext.registerService(Appender.class, appender, properties);
         }
     }
-
 }
