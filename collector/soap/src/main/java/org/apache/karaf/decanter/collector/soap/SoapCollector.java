ff11e9b04b0a61cbf49fc39b8a7f965d878efdd4
Jean-Baptiste Onofré <jb@nanthrax.net>
2019-02-17 22:05:47 +0100

    [KARAF-6118] Add SOAP collector

---
+++ collector/soap/src/main/java/org/apache/karaf/decanter/collector/soap/SoapCollector.java
@@ -0,0 +1,141 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.decanter.collector.soap;
+
+import org.apache.karaf.decanter.collector.utils.PropertiesPreparator;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component(
+        service = Runnable.class,
+        name = "org.apache.karaf.decanter.collector.soap",
+        immediate = true,
+        property = {
+                "decanter.collector.name=soap",
+                "scheduler.period:Long=60",
+                "scheduler.concurrent:Boolean=false",
+                "scheduler.name=decanter-collector-soap"
+        }
+)
+public class SoapCollector implements Runnable {
+
+    @Reference
+    public EventAdmin dispatcher;
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SoapCollector.class);
+
+    private URL url;
+    private String soapRequest;
+    private String topic;
+    private Dictionary<String, Object> config;
+
+    @Activate
+    public void activate(ComponentContext componentContext) throws Exception {
+        activate(componentContext.getProperties());
+    }
+
+    public void activate(Dictionary<String, Object> config) throws MalformedURLException {
+        this.config = config;
+        if (config.get("url") == null) {
+            throw new IllegalArgumentException("url property is mandatory");
+        }
+        this.topic = "decanter/collect/soap";
+        if (config.get("topic") != null) {
+            this.topic = (String) config.get("topic");
+        }
+        url = new URL((String) config.get("url"));
+        if (config.get("soap.request") == null) {
+            throw new IllegalStateException("soap.request property is mandatory");
+        }
+        soapRequest = (String) config.get("soap.request");
+    }
+
+    @Override
+    public void run() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("soap.request", soapRequest);
+        data.put("url", url);
+        data.put("type", "soap");
+
+        // custom fields
+        Enumeration<String> keys = config.keys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            data.put(key, config.get(key));
+        }
+
+        try {
+            PropertiesPreparator.prepare(data, config);
+        } catch (Exception e) {
+            LOGGER.warn("Can't prepare properties", e);
+        }
+
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestProperty("Content-Type", "text/xml");
+            connection.setRequestProperty("Accept", "text/xml");
+            try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())) {
+                writer.write(soapRequest);
+                writer.flush();
+                data.put("http.response.code", connection.getResponseCode());
+                data.put("http.response.message", connection.getResponseMessage());
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    StringBuffer buffer = new StringBuffer();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line).append("\n");
+                    }
+                    data.put("soap.response", buffer.toString());
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.warn("Can't request SOAP service", e);
+            data.put("error", e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        dispatcher.postEvent(new Event(topic, data));
+    }
+
+}
---
+++ collector/soap/src/test/java/org/apache/karaf/decanter/collector/soap/SoapCollectorTest.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.decanter.collector.soap;
+
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.service.event.Event;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class SoapCollectorTest {
+
+    private Server cxfServer;
+
+    @Before
+    public void setup() throws Exception {
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+
+        LoggingFeature loggingFeature = new LoggingFeature();
+        loggingFeature.setPrettyLogging(true);
+        factory.getFeatures().add(loggingFeature);
+
+        TestServiceImpl testService = new TestServiceImpl();
+        factory.setServiceBean(testService);
+        factory.setAddress("http://localhost:9090/test");
+        cxfServer = factory.create();
+        cxfServer.start();
+    }
+
+    @After
+    public void teardown() throws Exception {
+        cxfServer.stop();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBadConfiguration() throws Exception {
+        SoapCollector collector = new SoapCollector();
+        Dictionary<String, Object> config = new Hashtable<>();
+        collector.activate(config);
+    }
+
+    @Test
+    public void testWithInvalidRequest() throws Exception {
+        EventAdminMock eventAdminMock = new EventAdminMock();
+
+        SoapCollector collector = new SoapCollector();
+        Dictionary<String, Object> config = new Hashtable<>();
+        config.put("soap.request", "test");
+        config.put("url", "http://localhost:9090/test");
+        collector.dispatcher = eventAdminMock;
+        collector.activate(config);
+        collector.run();
+
+        Assert.assertEquals(1, eventAdminMock.postedEvents.size());
+
+        Event event = eventAdminMock.postedEvents.get(0);
+
+        Assert.assertEquals(500, event.getProperty("http.response.code"));
+        Assert.assertTrue(((String) event.getProperty("error")).contains("java.io.IOException: Server returned HTTP response code: 500 for URL: http://localhost:9090/test"));
+    }
+
+    @Test
+    public void testWithBadUrl() throws Exception {
+        EventAdminMock eventAdminMock = new EventAdminMock();
+
+        SoapCollector collector = new SoapCollector();
+        Dictionary<String, Object> config = new Hashtable<>();
+        config.put("soap.request", "test");
+        config.put("url", "http://foo.bar/foo");
+        collector.dispatcher = eventAdminMock;
+        collector.activate(config);
+        collector.run();
+
+        Assert.assertEquals(1, eventAdminMock.postedEvents.size());
+
+        Event event = eventAdminMock.postedEvents.get(0);
+        Assert.assertEquals("java.net.UnknownHostException: foo.bar", event.getProperty("error"));
+    }
+
+    @Test
+    public void testWithValidRequest() throws Exception {
+        EventAdminMock eventAdminMock = new EventAdminMock();
+
+        SoapCollector collector = new SoapCollector();
+        Dictionary<String, Object> config = new Hashtable<>();
+        config.put("url", "http://localhost:9090/test");
+        config.put("soap.request", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:soap=\"http://soap.collector.decanter.karaf.apache.org/\">\n" +
+                "   <soapenv:Header/>\n" +
+                "   <soapenv:Body>\n" +
+                "      <soap:echo>\n" +
+                "         <!--Optional:-->\n" +
+                "         <arg0>This is a test</arg0>\n" +
+                "      </soap:echo>\n" +
+                "   </soapenv:Body>\n" +
+                "</soapenv:Envelope>");
+        collector.dispatcher = eventAdminMock;
+        collector.activate(config);
+        collector.run();
+
+        Assert.assertEquals(1, eventAdminMock.postedEvents.size());
+
+        Event event = eventAdminMock.postedEvents.get(0);
+        Assert.assertNull(event.getProperty("error"));
+        Assert.assertEquals(200, event.getProperty("http.response.code"));
+        Assert.assertEquals("OK", event.getProperty("http.response.message"));
+        Assert.assertTrue(((String) event.getProperty("soap.response")).contains("hello This is a test"));
+    }
+
+}
